03dff77546b3f1bb4c429df49d45d76b62d15d3e
Paula <paulaeliegeferreira@gmail.com>
2022-10-03 16:10:28 -0300

    atualizando aula06

--- aula06/index.js
+++ aula06/index.js
@@ -1,18 +1,24 @@
 class Aluna {
-    constructor(nome, idade){
+    constructor(nome, idade) {
         this.nome = nome
         this.idade = idade
         this.notas = [8, 9, 8]
     }
 
-    pegarMedia (){
+    pegarMedia() {
         console.log(this.calcularMedia())
-    } 
+    }
 
-    calcularMedia(){
-        let somarNota = 0
-        this.notas.forEach(nota => {
-            
+    calcularMedia() {
+        let somarNotas = 0
+        this.notas.forEach(notas => {
+            somarNotas += notas
         })
+
+        return somarNotas / this.notas.length
     }
 }
+
+const aluna = new Aluna('Paula', 47)
+
+aluna.pegarMedia()
