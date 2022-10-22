14568cd0ac67d8db3f78a1cd2cf0e2a774a9747e
Paula <paulaeliegeferreira@gmail.com>
2022-10-22 08:55:03 -0300

    Mudando CEP

--- aula07/index.js
+++ aula07/index.js
@@ -1,38 +1,38 @@
-let nome = 'Paula'
-
-const dados = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-.then(resposta => resposta.json())
-.then(dados => console.log(dados))
-
-console.log(nome)
-
-function qualquer() {
-    return 2
-}
-
-console.log(qualquer())
-
-const curso = 'Pretalab'
-
-
-
 // let nome = 'Paula'
 
-// fetch('https://cep.awesomeapi.com.br/09791130')
+// const dados = fetch('https://pokeapi.co/api/v2/pokemon/ditto')
 // .then(resposta => resposta.json())
-// .then(dados => console.log(dados.address_nome))
+// .then(dados => console.log(dados))
 
 // console.log(nome)
 
 // function qualquer() {
-    
 //     return 2
-
 // }
-    
+
 // console.log(qualquer())
-    
+
 // const curso = 'Pretalab'
+
+
+
+let nome = 'Paula'
+
+fetch('https://cep.awesomeapi.com.br/02713000')
+.then(resposta => resposta.json())
+.then(dados => console.log(dados.address_nome))
+
+console.log(nome)
+
+function qualquer() {
+    
+    return 2
+
+}
+    
+console.log(qualquer())
+    
+const curso = 'Pretalab'
 
 // document.getElementById('aula').innerHTML = 'Trabalhando com Dom no JS'
 
